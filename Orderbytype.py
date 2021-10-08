438bee7135489c2f7ef2d9b1e3efb9bc2f13d82a
Danivdmijl <32570@ma-web.nl>
2021-10-08 09:29:01 +0200

    2de commit

--- Orderbytype.py
+++ Orderbytype.py
@@ -1,1 +1,13 @@
-#Les6
+stuff = ['c',5645,9393.77,"hello", True, False, "Good morning",88, -90, 777.777, 90,665.33,"F", ("abc") ]
+
+if type(stuff[0]) is str:   #type(stuff[0]) geeft het datatype van het eerste item in de list 
+    print("string found")
+
+if type(stuff[1]) is int:   #type(stuff[0]) geeft het datatype van het eerste item in de list 
+    print("interger found")
+
+if type(stuff[2]) is float:   #type(stuff[0]) geeft het datatype van het eerste item in de list 
+    print("float found")
+
+if type(stuff[3]) is bool:   #type(stuff[0]) geeft het datatype van het eerste item in de list 
+    print("booleans found")
