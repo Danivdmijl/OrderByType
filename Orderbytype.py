9f6be78a4edf57550a97956854d3a3b7701e89b7
Danivdmijl <32570@ma-web.nl>
2021-10-26 12:54:50 +0200

    Commit

--- Orderbytype.py
+++ Orderbytype.py
@@ -2,24 +2,24 @@ import time
 
 
 while True:
-    stuff = [56,5645,9393.77,"hello", True, False, "Good morning",88, -90, 777.777, 90,665.33,"F", ("abc") ]
+    stuff = ["hello", True, False, "Good morning",88, -90, 777.777, 90,665.33,"F", ("abc") ]
     break
 if type(stuff[0]) is str:
     print("string found")
 else:
     print("string not found...")
 
-if type(stuff[1]) is int:   
+if type(stuff[4]) is int:   
     print("interger found")
 else:
     print("interger not found...")
 
-if type(stuff[2]) is float:
+if type(stuff[6]) is float:
     print("float found")
 else:
     print("float not found...")
 
-if type(stuff[4]) is bool:
+if type(stuff[1]) is bool:
     print("booleans found")
 else:
     print("booleans not found...")
